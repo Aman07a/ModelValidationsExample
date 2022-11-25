2cc802c92ca2c70c81d0d1e61bec6eb50b5f3f8e
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-25 20:10:37 +0100

    All Model Validations - Part 1

--- ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/Models/Person.cs
@@ -4,13 +4,17 @@ namespace ModelValidationsExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Person Name can't be empty or null")]
+        [Required(ErrorMessage = "{0} can't be empty or null")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
         public string? PersonName { get; set; }
 
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} should be between ${1} and {2}")]
         public double? Prize { get; set; }
 
         public override string ToString()
