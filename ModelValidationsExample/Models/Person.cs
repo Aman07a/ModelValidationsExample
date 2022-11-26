2a5532ed2ef8165b049af825ca4cd1feb3f2da28
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-26 08:47:46 +0100

    IValidatableObject

--- ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/Models/Person.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationsExample.Models
 {
-    public class Person
+    public class Person : IValidatableObject
     {
         [Required(ErrorMessage = "{0} can't be empty or null")]
         [Display(Name = "Person Name")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
-        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets. space and dot(.)")]
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets. space and dot(.)")]
         public string? PersonName { get; set; }
 
         [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
@@ -40,9 +40,19 @@ namespace ModelValidationsExample.Models
         [DateRangeValidator("FromDate", ErrorMessage = "'From Date' should be older than or equal to 'To Date'")]
         public DateTime? ToDate { get; set; }
 
+        public int? Age { get; set; }
+
         public override string ToString()
         {
             return $"Person object - Person name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, Confirm Password: {ConfirmPassword}, Prize: {Price}";
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DateOfBirth.HasValue == false && Age.HasValue == false)
+            {
+                yield return new ValidationResult("Either of Date of Birth or Age must be supplied", new[] { nameof(Age) });
+            }
         }
     }
 }
